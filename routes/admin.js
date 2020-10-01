2d75e0e447140872d300cf0da9c0c17ddd9a81ff
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-10-01 23:59:10 +0530

    form creation

--- routes/admin.js
+++ routes/admin.js
@@ -35,4 +35,9 @@ router.get('/', function(req, res, next) {
   res.render('admin/view-products',{admin:true,products})
 });
 
+router.get('/add-product',function(req,res){
+  res.render('admin/add-product')
+
+})
+
 module.exports = router;
