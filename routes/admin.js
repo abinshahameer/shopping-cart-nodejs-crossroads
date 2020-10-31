3d60c1a8417841562fcc6ad6223aff14611c415d
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-10-31 17:20:34 +0530

    part17 finished

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -42,6 +44,17 @@ router.get('/add-product',function(req,res){
 router.post('/add-product',(req,res)=>{
   console.log(req.body)
   console.log(req.files.Image)
+  productHelpers.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product")
+      }else{
+        console.log(err);
+      }
+    })
+    
+  })
 })
 
 module.exports = router;
